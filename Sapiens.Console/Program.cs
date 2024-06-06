da952f92dc2e761bbb0aa74dc9f25575e8a95a06
Liluyoud Cury de Lacerda <liluyoud@hotmail.com>
2024-06-05 20:57:55 -0400

    Menu

--- Sapiens.Console/Program.cs
+++ Sapiens.Console/Program.cs
@@ -1,12 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Sapiens.Shared.Contexts;
-using Sapiens.Shared.Entities;
 
 var context = new SapiensContext();
 
 FazerMigracao();
+Menu();
 
-CriarTitulo("Sapiens - Sistema Acadêmico");
 
 void FazerMigracao()
 {
@@ -17,18 +16,78 @@ void FazerMigracao()
     Console.ReadLine();
 }
 
+void Menu()
+{
+    CriarTitulo("Sapiens - Sistema Acadêmico");
+    var opcao = "";
+    Console.WriteLine(" [1] Cursos");
+    Console.WriteLine(" [2] Disciplinas");
+    Console.WriteLine(" [3] Professores");
+    Console.WriteLine(" [4] Alunos");
+    Console.WriteLine(" [5] Matricula");
+    Console.WriteLine("\n [9] Sair");
+    Console.WriteLine("-----------------------------------");
+    Console.Write(" \nOpção:");
+    opcao = Console.ReadLine();
+    switch (opcao)
+    {
+        case "1": MenuAluno(); break;
+        case "2": MenuAluno(); break;
+        case "3": MenuAluno(); break;
+        case "4": MenuAluno(); break;
+        case "5": MenuAluno(); break;
+    }
+}
+
+void MenuAluno()
+{
+    CriarTitulo("Sapiens - Cadastro de Alunos");
+    var opcao = "";
+    Console.WriteLine(" [1] Consultar");
+    Console.WriteLine(" [2] Adicionar");
+    Console.WriteLine(" [3] Remover");
+    Console.WriteLine("\n [9] Voltar");
+    Console.WriteLine("-----------------------------------");
+    Console.Write(" \nOpção:");
+    opcao = Console.ReadLine();
+    switch (opcao)
+    {
+        case "1": ConsularAluno(); break;
+        case "2": AdicionarAluno(); break;
+        case "3": RemoverAluno(); break;
+        case "9": Menu(); break;
+    }
+    MenuAluno();
+}
+
+void ConsularAluno()
+{
+    CriarTitulo("Sapiens - Consultar Aluno");
+    Console.WriteLine("-----------------------------------");
+    Console.WriteLine("[Enter] para continuar");
+    Console.ReadLine();
+}
+
+void AdicionarAluno()
+{
+    CriarTitulo("Sapiens - Adicionar Aluno");
+    Console.WriteLine("-----------------------------------");
+    Console.WriteLine("[Enter] para continuar");
+    Console.ReadLine();
+}
+
+void RemoverAluno()
+{
+    CriarTitulo("Sapiens - Remover Aluno");
+    Console.WriteLine("-----------------------------------");
+    Console.WriteLine("[Enter] para continuar");
+    Console.ReadLine();
+}
+
 void CriarTitulo(string titulo)
 {
     Console.Clear();
     Console.WriteLine("-----------------------------------");
     Console.WriteLine($" {titulo}");
     Console.WriteLine("-----------------------------------");
-}
-
-void AdicionaAluno()
-{
-    CriarTitulo("Sapiens - Adicionar Aluno");
-    Console.WriteLine("[Enter] para continuar");
-    Console.ReadLine();
-
 }
