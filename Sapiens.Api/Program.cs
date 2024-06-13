409e4ebe76bc93fd0fbf1a654993990b4aeb3c0a
Liluyoud Cury de Lacerda <liluyoud@hotmail.com>
2024-06-12 21:00:49 -0400

    api academico

---
+++ Sapiens.Api/Program.cs
@@ -0,0 +1,59 @@
+using Microsoft.EntityFrameworkCore;
+using Sapiens.Shared.Contexts;
+using Sapiens.Shared.Entities;
+using Sapiens.Shared.Enums;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<SapiensContext>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/cursos", (SapiensContext context) =>
+{
+    var cursos = context.Cursos.OrderBy(c => c.Nome).ToList();
+    return Results.Ok(cursos);
+}).Produces<IEnumerable<Curso>>().WithOpenApi();
+
+app.MapGet("/cursos/{id}", (int id, SapiensContext context) =>
+{
+    var curso = context.Cursos.Find(id);
+    return curso == null ? Results.NotFound() : Results.Ok(curso);
+}).Produces<Curso>().WithOpenApi();
+
+app.MapGet("/cursos/pornome/{nome}", (string nome, SapiensContext context) =>
+{
+    var cursos = context.Cursos.Where(c => c.Nome.ToLower().Contains(nome.ToLower())).ToList();
+    return cursos == null ? Results.NotFound() : Results.Ok(cursos);
+}).Produces<IEnumerable<Curso>>().WithOpenApi();
+
+app.MapPost("/cursos", (Curso curso, SapiensContext context) =>
+{
+    context.Add(curso);
+    context.SaveChanges();
+    return Results.Ok(curso);
+}).Produces<Curso>().WithOpenApi();
+
+app.MapDelete("/cursos", (int id, SapiensContext context) =>
+{
+    var curso = context.Cursos.Find(id);
+    if (curso != null)
+    {
+        context.Remove(curso);
+        context.SaveChanges();
+    }
+    return Results.NoContent();
+}).WithOpenApi();
+
+app.Run();
+
--- Sapiens.Console/Program.cs
+++ Sapiens.Console/Program.cs
@@ -1,6 +1,25 @@
-﻿using Sapiens.Shared.Helpers;
+﻿using Sapiens.Shared.Models;
+using System.Text.Json;
 
-ConsoleHelper.Menu();
+Console.Write("Informe o CEP: ");
+var cep = Console.ReadLine();
+
+var http = new HttpClient();
+var jsonCep = await http.GetStringAsync($"https://viacep.com.br/ws/{cep}/json/");
+var endereco =  JsonSerializer.Deserialize<CepModel>(jsonCep, new JsonSerializerOptions()
+{
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+});
+if (endereco != null)
+{
+    Console.WriteLine($"Endereço: {endereco.Logradouro}");
+    Console.WriteLine($"Bairro: {endereco.Bairro}");
+    Console.WriteLine($"Cidade: {endereco.Localidade}");
+    Console.WriteLine($"Estado: {endereco.Uf}");
+    Console.WriteLine($"DDD: {endereco.Ddd}");
+}
+
+//ConsoleHelper.Menu();
 
 
 
